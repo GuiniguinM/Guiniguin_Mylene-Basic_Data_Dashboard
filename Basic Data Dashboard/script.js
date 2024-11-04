2771fe4793caa6adf1069a82d8176bfa27dcb77d
GuinM <guiniguinmylene123@gmail.com>
2024-11-04 22:53:04 +0800

    CIT17

--- Basic Data Dashboard/script.js
+++
@@ -1,9 +0,0 @@
-const totalUsers=document.getElementById('totalUsers');
-const totalSales=document.getElementById('totalSales');
-const alertButton=document.getElementById('alertButton');
-
-alertButton.addEventListener('click',()=>{
-    const users=totalUsers.textContent;
-    const sales=totalSales.textContent;
-    alert(`Total Users: ${users}, Total Sales: ${sales}`);
-});
